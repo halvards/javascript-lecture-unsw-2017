ed2a80181c410ad1064432d97c4ff843985e13bd
Halvard Skogsrud <halvard@skogsrud.com>
2017-09-03 17:27:38 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Slides for university guest lecture on JavaScript, available here:
+<http://halvards.github.io/javascript-lecture-unsw-2016/>
+
+Delivered on Monday 4 September 2017.
+
+Usage:
+
+* Press `p` to toggle presenter notes
+* Press `w` to toggle widescreen mode
+* Press `h` to toggle code highlighting
+* Press `f` to toggle code full screen mode
+* Append `?presentme=true` to turn on presentation mode with two browser windows. If you navigate in the presenter's screen, the main slide screen will move along. This setting is sticky, to append `?presentme=false` to turn off.
---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Learn JavaScript <br>- the right way!',
+    subtitle: '',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/brackets_color_24x24.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Halvard Skogsrud',
+    company: 'IAG',
+    twitter: '@halvards',
+    github: 'https://github.com/halvards'
+  }]
+};
